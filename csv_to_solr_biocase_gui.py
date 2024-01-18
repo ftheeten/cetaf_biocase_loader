b51d5b02bfeaefde4100d9bec91143456de4ab9d
Franck Theeten <franck.theeten@africamuseum.be>
2024-01-18 18:26:37 +0100

    Add files via upload

--- csv_to_solr_biocase_gui.py
+++ csv_to_solr_biocase_gui.py
@@ -1,5 +1,6 @@
 import pyodbc
 import httplib2
+from urllib import parse
 import datetime
 import traceback
 import sys
@@ -27,7 +28,39 @@ SOLR_AUTH=False
 SOLR_USER=""
 SOLR_PWD=""
 
+CHECK_URL="select?indent=true&q.op=OR&q="
+CHECK_FIELD="geocase_id"
+
 field_mapping={}
+
+
+def print_time():
+    now = datetime.datetime.now()
+    print ("Current date and time : ")
+    print (now.strftime("%Y-%m-%d %H:%M:%S"))
+    
+    
+def check_already_exists(p_solr_endpoint, p_check_query, p_fieldname, p_key):
+    #print(p_key)
+    encoded_key=parse.quote_plus(p_key)
+    #print(encoded_key)
+    check_url=p_solr_endpoint+p_check_query+p_fieldname+"%3A"+encoded_key
+    #print(check_url)
+    p_h = httplib2.Http(".cache")
+    resp, content = p_h.request(check_url, "GET" )
+    #print(resp)
+    #print(content)
+    content_dict=json.loads(content)
+    returned= None
+    if "response" in content_dict:
+        if "docs" in content_dict["response"]:
+            if len(content_dict["response"]["docs"])>0:
+                tmp_doc=content_dict["response"]["docs"][0]
+                if "id" in tmp_doc:
+                    returned=tmp_doc["id"]
+    #print("======================>")
+    #print(returned)
+    return returned
 
 def add_solr(p_solr_url,  p_fields, list_multi_fields=None , p_auth=False, p_solr_user=None, p_solr_password=None ):
     p_h = httplib2.Http(".cache")
@@ -73,7 +106,7 @@ def get_encoding(p_file):
             if detector.done:
                 break
     detector.close()
-    print(detector.result)
+    #print(detector.result)
     return detector.result["encoding"] or ""
 
 def create_mapping(df_mapping):
@@ -93,19 +126,19 @@ def create_mapping(df_mapping):
 
 def parse_csv(p_src, p_mapping, p_solr_endpoint, p_auth=False, p_solr_user=None, p_solr_password=None):
     global field_mapping
-    print(p_src)
-    print(p_mapping)
+    global CHECK_FIELD
+    global CHECK_URL
     encoding_mapping=get_encoding(p_mapping)
     df_mapping=pnd.read_csv(p_mapping, sep="\t", header=0, encoding=encoding_mapping)
     create_mapping(df_mapping)
-    print(field_mapping)
     
     encoding_src=get_encoding(p_src)
     df_src=pnd.read_csv(p_src, sep="\t", header=0,encoding=encoding_src)
     df_src=df_src.replace(np.nan, "")
     
+    print("Got main data")
+    print_time()
     for index, row in df_src.iterrows():
-        print(index)
         #print(row)
         doc={}
         for src_field, key_array in field_mapping.items():            
@@ -114,8 +147,15 @@ def parse_csv(p_src, p_mapping, p_solr_endpoint, p_auth=False, p_solr_user=None,
                 
                 for solr_field in key_array:
                     doc[solr_field]=val
-        print(doc)
-        add_solr(p_solr_endpoint, doc, list_multi_fields=None , p_auth=False, p_solr_user=None, p_solr_password=None )
+        if CHECK_FIELD in doc:
+            exist_id=check_already_exists(p_solr_endpoint, CHECK_URL, CHECK_FIELD, doc[CHECK_FIELD]) or ""
+            if len(exist_id)>0:
+                doc["id"]=exist_id
+            add_solr(p_solr_endpoint, doc, list_multi_fields=None , p_auth=False, p_solr_user=None, p_solr_password=None )
+        if index%100==0:
+            print(index)
+    print("done")
+    print_time()
         
 def load_data():
     global MAPPING_FILE
@@ -189,7 +229,7 @@ def start():
     
     but_proceed=QPushButton('Load data')
     layout.addWidget(but_proceed)
-    but_file_src.clicked.connect(load_data)
+    but_proceed.clicked.connect(load_data)
     
     
     
